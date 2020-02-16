ea7d689773a9a4d49b3f0fbc59ac954666d10586
Ovidiu Bute <ovidiu.bute@gmail.com>
2020-02-16 21:09:57 +0200

    Increase layout width.

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Container = styled.section`
 margin: 0 auto;
-max-width: 870px;
+max-width: 1280px;
 `;
 
 export default Container;
--- src/components/Header/Wrapper.js
+++ src/components/Header/Wrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.header`
   margin: 0 auto;
   max-width: 650px;
-  padding: 0 50px 50px;
+  padding: 0 25px 25px;
   text-align: center;
 `;
 
