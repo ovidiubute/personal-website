adedbc134a76cbf6177310ba764127be72e86906
GitButler <gitbutler@gitbutler.com>
2024-02-29 10:44:19 +0200

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - Virtual branch (refs/gitbutler/Virtual-branch)
       branch head: 7291e78e9cd7d2d68a4f3f4e2d4bd1a2018ae672
       - gatsby-config.js
       - yarn.lock
       - package.json
    
    Your previous branch was: refs/heads/master
    
    The sha for that commit was: 599fb49926f132177bc4bfe0e0e3b3f0bf937703
    
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,42 +1,71 @@
 module.exports = {
-    plugins: [
-        {
-            resolve: "gatsby-theme-developer",
-            options: {
-                title: "Ovidiu Bute (Ovi)",
-                subtitle: "Tech Lead",
-                description: "Ovi's personal blog and portfolio",
-                siteUrl: "https://ovidiu.dev",
-                defaultPreviewImage: "/favicon-32x32.png",
-                author: {
-                    name: "Ovidiu Bute (Ovi)",
-                    bio: "Tech Lead",
-                    avatar: "/avatar.jpeg",
-                    links: {
-                        linkedin: "https://linkedin.com/in/ovidiubute",
-                        github: "https://github.com/ovidiubute",
-                        mail: "hello@ovidiu.dev",
-                        twitter: "",
-                        instagram: "",
-                    }
-                },
-                links: [
-                    {
-                        title: "Home",
-                        href: "/"
-                    },
-                    {
-                        title: "About me",
-                        href: "/about-me"
-                    },
-                    {
-                        title: "Blog",
-                        href: "/blog"
-                    }
-                ],
-                articlesPerPage: 4,
-                language: "en"
-            },
+  plugins: [
+    {
+      resolve: "gatsby-theme-developer",
+      options: {
+        title: "Ovidiu Bute (Ovi)",
+        subtitle: "Tech Lead",
+        description: "Ovi's personal blog and portfolio",
+        siteUrl: "https://ovidiu.dev",
+        defaultPreviewImage: "/favicon-32x32.png",
+        author: {
+          name: "Ovidiu Bute (Ovi)",
+          bio: "Tech Lead",
+          avatar: "/avatar.jpeg",
+          links: {
+            linkedin: "https://linkedin.com/in/ovidiubute",
+            github: "https://github.com/ovidiubute",
+            mail: "hello@ovidiu.dev",
+            twitter: "",
+            instagram: "",
+          },
         },
-    ],
-}
+        links: [
+          {
+            title: "Home",
+            href: "/",
+          },
+          {
+            title: "About me",
+            href: "/about-me",
+          },
+          {
+            title: "Blog",
+            href: "/blog",
+          },
+        ],
+        articlesPerPage: 4,
+        language: "en",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-138322227-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+          // Defaults to https://www.googletagmanager.com
+          origin: "YOUR_SELF_HOSTED_ORIGIN",
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 0,
+        },
+      },
+    },
+  ],
+};
