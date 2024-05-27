70e6f47223b75b18255cbc1fb09cb96d468a0e68
Ovidiu (Ovi) <ovidiubute@users.noreply.github.com>
2024-05-27 09:19:09 +0300

    Fix tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GTM-MWW7KJ4F", // Google Analytics / GA
+          "G-JTC98N8R71", // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
